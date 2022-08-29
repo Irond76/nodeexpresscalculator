8540780805a9aba0594f24b8269be6e5bff680a2
Dale Bendixen <dalebendixen@gmail.com>
2022-08-28 19:43:21 -0600

    Add Calculator Complete

--- calculator.js
+++ calculator.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res) => {
-    res.send(`<h1>Hello World!!!</h1>`)
-})
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/', (req, res) => {
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let result = num1 + num2;
+    res.send(`The Result Of The Addition is: ${result}`);
+    
+}); 
+    
+
+
+
 
 
 
